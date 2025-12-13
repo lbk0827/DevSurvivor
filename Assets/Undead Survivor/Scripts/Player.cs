2c7fafd48a9fd47ccc6cc4696a81cf1c58a7e2cb
이병권 <bk@ibyeong-gwon-ui-MacBookAir.local>
2025-12-13 22:54:04 +0900

    feat : Project Setting
    - 프로젝트 세팅을 위해 초기 Asset 파일 커밋

---
+++ Assets/Undead Survivor/Scripts/Player.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 inputVec;
+    public float speed;
+
+    Rigidbody2D rigid;
+    SpriteRenderer spriter;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputVec.x = Input.GetAxisRaw("Horizontal");
+        inputVec.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        // 1. 힘을 준다 (학습용)
+        //rigid.AddForce(inputVec);
+
+        // 2. 속도 제어 (학습용)
+        //rigid.velocity = inputVec;
+
+        // 3. 위치 이동
+        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
+    }
+
+    void LateUpdate()
+    {
+        if (inputVec.x != 0) {
+            spriter.flipX = inputVec.x < 0;
+        }   
+    }
+}
