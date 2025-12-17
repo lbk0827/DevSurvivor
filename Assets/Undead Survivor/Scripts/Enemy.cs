75718b0302ac93f9e47d90a9f9b4b764596c74e7
이병권 <bk@ibyeong-gwon-ui-MacBookAir.local>
2025-12-18 01:22:11 +0900

    fix: Enemy !IsLive 코드 추가
    - 살아있지 않는 경우에는 실행하지 않고 그냥 나갑니다 코드 추가
    - 테스트 용이기 때문에 항상 살아있다는 것으로 처리하기 위해 bool IsLive = true; 처리 해놓음.

--- Assets/Undead Survivor/Scripts/Enemy.cs
+++ Assets/Undead Survivor/Scripts/Enemy.cs
@@ -8,7 +8,8 @@ public class Enemy : MonoBehaviour
     public float speed;
     public Rigidbody2D target;   //물리적으로 따라갈거니까 Rigidbody2d Target 설정.
 
-    bool isLive; //이 몬스터가 살아있는지 죽어있는지 분별해주는 isLive라는 bool 변수까지 추가.
+    bool isLive = true; //이 몬스터가 살아있는지 죽어있는지 분별해주는 isLive라는 bool 변수까지 추가.
+    // 테스트 용이라서 "= true"를 추가해두었음. 나중에 수정 필요.
 
     //리지드바디2D와 스프라이트렌더러를 위한 변수 선언
     Rigidbody2D rigid;
@@ -25,6 +26,11 @@ public class Enemy : MonoBehaviour
     // 물리적인 이동이기 때문에 우리는 Update를 쓰지 않고, FixedUpdate();를 사용한다.
     void FixedUpdate() 
     {
+        // 몬스터가 살아있는 동안에만 움직이도록 조건 추가.
+        // "isLive가 아니다"일 경우에는 실행시키지 말고 그냥 나가요 (return;) 이라는 뜻이죠.
+        if (!isLive)
+            return;
+
         //벡터의 연산으로 몬스터가 이동하는 방향 (플레이어가 있는 쪽으로 향함)을 구할 것임.
         Vector2 dirVec = target.position - rigid.position;
         Vector2 nextVec = dirVec.normalized * speed * Time.fixedDeltaTime;
@@ -35,7 +41,13 @@ public class Enemy : MonoBehaviour
         // 프레임의 영향으로 결과가 달라지지 않도록 FixedDeltaTime 사용.
         // 플레이어의 키입력 값을 더한 이동 = 몬스터의 방향 값을 더한 이동. 이라고 말할 수 있음.
         // rigid에 velocity 값이 존재하면 충돌 시 멀리 날아가버리는데, 물리 속도가 이동에 영향을 주지 안도록 속도 제거 (velocity = vector2.zero)
-        
+
     }
+    void LateUpdate()
+    {
+        // 목표의 X축 값과 자신의 X축 값을 비교하여 작으면 true가 되도록 설정.
+        spriter.flipX = target.position.x < rigid.position.x; 
+    }
+
 
 }
