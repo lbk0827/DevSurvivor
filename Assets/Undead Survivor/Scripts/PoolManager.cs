5370b979a0ebd83ba0b8d8008ffc85248694983f
이병권 <bk@ibyeong-gwon-ui-MacBookAir.local>
2025-12-21 23:01:12 +0900

    feat: 풀링 함수 작성

--- Assets/Undead Survivor/Scripts/PoolManager.cs
+++ Assets/Undead Survivor/Scripts/PoolManager.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PoolManager : MonoBehaviour
@@ -19,7 +21,38 @@ public class PoolManager : MonoBehaviour
         for (int index = 0; index < pools.Length; index++) {// index는 0부터 시작합니다.
         pools[index] = new List<GameObject>();
 
-        Debug.Log(pools.Length);
         }
     }
+    // 게임 오브젝트를 반환하는 함수 선언 //  어떤 풀에 들어있는 오브젝트를 가져올 것이냐? -> 가져올 오브젝트 종류를 결정하는 매게 변수 추가.
+    public GameObject Get(int index)
+    {
+        //게임 오브젝트 지역 변수와 리턴을 미리 작성.
+        GameObject select = null;
+
+
+        //... 선택한 풀의 놀고있는 (=비활성화된) 게임오브젝트 접근
+        foreach (GameObject item in pools[index])
+        {   
+            if (!item.activeSelf) {//내용물 오브젝트가 비활성화(대기 상태)인지 확인.
+            //... 발견하면 select 변수에 할당(=해당 변수에 집어 넣게는 소리.)       
+            select = item;
+            select.SetActive(true);
+            break;     
+            }
+        }
+
+        //... 못 찾았으면..?
+        if (!select)
+            {
+             //... 새롭게 생성하고 select 변수에 할당.// Instantiate는 오리지널 게임 오브젝트를 하나 복자해서 장면에 딱 둔다.
+             //... 생성된 오브젝트는 해당 오브젝트 풀 리스트에 Add 함수로 추가.
+            select = Instantiate(prefabs[index], transform);
+            pools[index].Add(select);
+            }
+
+
+        // foreach : 배열, 리스트들의 데이터를 순차적으로 접근하는 반복문.
+
+        return select;
+        }
 }
