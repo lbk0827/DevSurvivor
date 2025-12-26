4bf5336d53bee4e7dc1b0fd000b930f2d208de84
이병권 <bk@ibyeong-gwon-ui-MacBookAir.local>
2025-12-26 22:45:42 +0900

    fix: Spawner.cs 불필요한 코드 정리 (Claude)
    - 클로드가 불필요한 코드 삭제해줌.

--- Assets/Undead Survivor/Scripts/Spawner.cs
+++ Assets/Undead Survivor/Scripts/Spawner.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR;
 
 public class Spawner : MonoBehaviour
 {
@@ -25,7 +21,7 @@ public class Spawner : MonoBehaviour
         timer += Time.deltaTime;
         level = Mathf.Min(Mathf.FloorToInt(GameManager.instance.gameTime / 10f), spawnData.Length - 1);
 
-        if (timer > (spawnData[level].spawnTime)) {
+        if (timer > spawnData[level].spawnTime) {
             timer = 0;
             Spawn();
         }  
@@ -34,7 +30,7 @@ public class Spawner : MonoBehaviour
     void Spawn()
     {
         GameObject enemy = GameManager.instance.pool.Get(0);
-        enemy.transform.position = spawnPoint[Random.Range(1, spawnPoint.Length)].position;
+        enemy.transform.position = spawnPoint[UnityEngine.Random.Range(1, spawnPoint.Length)].position;
         enemy.GetComponent<Enemy>().Init(spawnData[level]);
     }
 }
